eb6cac89a0e874045849e3b064ce44270da75d9d
Tw1ddle <sam@silver42.karoo.co.uk>
2012-12-19 15:05:10 +0000

    Fixed mistakes up to August and the December puzzle. Set web/email/feedback addresses. Added initial help and tutorial files.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
+    //todo - parse command line arguments for opening associated crossword files etc
+
     w.show();
     
     return a.exec();
